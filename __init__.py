eda5f2a92a50957dfc101c199346be93bb84490a
Ash9944 <89572794+Ash9944@users.noreply.github.com>
2021-08-29 18:52:00 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from class1 import Employee
+from class2 import Parent
+e= Employee("ashwajit","15000")
+p = Parent()
+e.displayEmployee()
+e.displayCount()
+p.parentMethod()
+p.setAttr("200")
+p.getAttr()
